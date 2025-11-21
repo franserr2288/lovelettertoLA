c406cc6855e13dca3421e5e143dc504cf903873b
francisco laptop <franserr2288@outlook.com>
2025-11-21 10:47:17 -0800

    architecture changes, move from timer trigger on ingestion to abstraction over the socrata api endpoint with an ingestion function, each run will do either a csv push or the parquet but not both at the same time. if i hit scalability ceiling faster than i assume i will then i'll use the queue mechanism i introduced to do a lightweight self-feeding/state machine pattern with built in retries. needed to seperate this at the infra level so that i can reuse that ingestion function for datasets smaller than 1GB

--- src/ingestion/city311/ingest_city311.py
+++ src/ingestion/city311/ingest_city311.py
@@ -1,12 +1,19 @@
-import requests, os
-from dotenv import load_dotenv
+import requests, os, json
 import awswrangler as wr 
 import pandas as pd
 
-# load_dotenv()
-
 
 def handler(event, context):
+    body = json.loads(event["Records"][0]["body"])
+    dataset_name = body["DATASET_NAME"]
+    dataset_resource_id = body["DATASET_RESOURCE_ID"]
+    format = body["FORMAT"]
+
+    assert format
+    assert dataset_name 
+    assert dataset_resource_id
+
+
     url = construct_url_for_full_dataset_json()
     headers = {
         'Content-Type': 'application/json',
@@ -19,16 +26,16 @@ def handler(event, context):
     data_frame = pd.DataFrame(data)
     bucket_name = os.environ["BUCKET_NAME"]
 
-    raw_data_path = f"s3://{bucket_name}/{os.environ["DATASET_NAME"]}/raw/data.csv"
-    parquet_path = f"s3://{bucket_name}/{os.environ["DATASET_NAME"]}/parquet/"
+    raw_data_path = f"s3://{bucket_name}/{dataset_name}/raw/data.csv"
+    parquet_path = f"s3://{bucket_name}/{dataset_resource_id}/parquet/"
     
-    wr.s3.to_csv(df=data_frame, path=raw_data_path, index=False)
-    wr.s3.to_parquet(df=data_frame, dataset=True, path=parquet_path, max_rows_by_file=100000)
+    if format == "CSV":
+        wr.s3.to_csv(df=data_frame, path=raw_data_path, index=False)
+    else:
+        wr.s3.to_parquet(df=data_frame, dataset=True, path=parquet_path, max_rows_by_file=100000)
     
 
 def construct_url_for_full_dataset_json():
     base_url = os.environ["BASE_URL"]
     resource_id = os.environ["DATASET_RESOURCE_ID"]
     return base_url+"/api/v3/views/"+resource_id+"/query.json"
-
-handler({},{})
---
+++ src/ingestion/orchestrator/orchestrator.py
@@ -0,0 +1,19 @@
+import boto3, json
+
+def handler(event, context):
+    queue_name = event["QUEUE_NAME"]
+    dataset_name = event["DATASET_NAME"]
+    dataset_resource_id = event["DATASET_RESOURCE_ID"]
+    format = event["FORMAT"]
+
+
+    sqs_client = boto3.client("sqs")
+    response = sqs_client.get_queue_url(
+        QueueName=queue_name
+    )
+    queue_url = response["QueueUrl"]
+
+    sqs_client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps({"DATASET_NAME":dataset_name, "DATASET_RESOURCE_ID":dataset_resource_id, "FORMAT":format}),
+    )
