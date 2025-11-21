ee9c06bfc4a687b6d03fa6b89202f0797c65ab8f
francisco laptop <franserr2288@outlook.com>
2025-11-21 01:19:14 -0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -5,4 +5,9 @@ aws cloudformation create-stack \
   --parameters \
     ParameterKey=GitHubOrg,ParameterValue=franserr2288 \
     ParameterKey=GitHubRepo,ParameterValue=lovelettertoLA \
-  --capabilities CAPABILITY_NAMED_IAM
+  --capabilities CAPABILITY_NAMED_IAM \
+  --profile local-dev \
+  --region us-east-1
+
+# get the arn and put it in gh secrets or whatever secret vendor you want to use 
+# now our actions can assume the role
