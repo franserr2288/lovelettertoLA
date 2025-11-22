3b9725f604fd158f43c243ca8fcde70fcc5bcfcd
francisco laptop <franserr2288@outlook.com>
2025-11-21 23:32:46 -0800

    oidc set up command/script

---
+++ setup/oidc.sh
@@ -0,0 +1,10 @@
+aws cloudformation create-stack \
+  --stack-name github-oidc-setup \
+  --template-body file://iac/iam/github-oidc-setup.yaml \
+  --parameters \
+    ParameterKey=GitHubOrg,ParameterValue=franserr2288 \
+    ParameterKey=GitHubRepo,ParameterValue=lovelettertoLA \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --profile local-dev \
+  --region us-east-1
+ 
