9b750f2ef8e6d48fecc16fb0ce83687f0846b76d
francisco laptop <franserr2288@outlook.com>
2025-11-21 21:30:57 -0800

    add logic to get secret from param store

--- src/ingestion/city311/ingest_city311.py
+++ src/ingestion/city311/ingest_city311.py
@@ -1,3 +1,4 @@
+import boto3
 import requests, os, json
 import awswrangler as wr 
 import pandas as pd
@@ -9,10 +10,11 @@ def handler(event, context):
     dataset_resource_id = body["DATASET_RESOURCE_ID"]
     format = body["FORMAT"]
 
-    url = construct_url_for_full_dataset_json()
+
+    url = construct_url_for_full_dataset_json(dataset_resource_id)
     headers = {
         'Content-Type': 'application/json',
-        'X-App-Token': os.environ["SOCRATA_APP_TOKEN"],
+        'X-App-Token': get_app_token(),
     }
 
     response = requests.get(url=url, headers=headers)
@@ -33,3 +35,16 @@ def handler(event, context):
 def construct_url_for_full_dataset_json(dataset_resource_id):
     base_url = os.environ["BASE_URL"]
     return base_url + "/api/v3/views/" + dataset_resource_id + "/query.json"
+
+def get_app_token():
+    socrata_app_token_param_name = os.environ["SOCRATA_TOKEN_PARAM_NAME"]
+    return get_secret_from_ssm(socrata_app_token_param_name, True)
+
+def get_secret_from_ssm(param_name, is_secure_string=True):
+    ssm = boto3.client('ssm')
+    response = ssm.get_parameter(
+        Name=param_name,
+        WithDecryption=is_secure_string
+    )
+    return response['Parameter']['Value']
+    
