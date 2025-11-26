741dceda473beb25c27eefab9c0ea226dfb9a2cb
francisco laptop <franserr2288@outlook.com>
2025-11-25 16:11:47 -0800

    add deployment cmd for cf template

--- setup/oidc.sh
+++ setup/oidc.sh
@@ -8,3 +8,12 @@ aws cloudformation create-stack \
   --profile local-dev \
   --region us-east-1
  
+aws cloudformation deploy \                           
+  --stack-name github-oidc-setup \
+  --template-file iac/iam/github/github-oidc-setup.yaml \
+  --parameter-overrides \
+    GitHubOrg=franserr2288 \
+    GitHubRepo=lovelettertoLA \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --profile local-dev \
+  --region us-east-1
