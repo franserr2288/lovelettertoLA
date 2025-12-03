f87637f87024656297b6346ebbdf45788e2c2932
francisco laptop <franserr2288@outlook.com>
2025-12-03 01:13:56 -0800

    simplify logic

--- src/data_aggregator/orchestrator/fan_in.py
+++ src/data_aggregator/orchestrator/fan_in.py
@@ -1,51 +1,39 @@
-import datetime
 import json
-import os
-import awswrangler as wr
 import boto3
+from shared.messages.sqs import get_queue_url
+from shared.models.tables import JobBatch, JOB_BATCH_SK
 from shared.utils.logging.logger import setup_logger
 
+
+from pynamodb.expressions.update import Action
+from pynamodb.expressions.operand import Value
+
 logger = setup_logger(__name__)
-
-
-
-POLL_DELAY_SECONDS = 60  # check S3 every minute
-MAX_POLL_DURATION_MINS = 60  # kill switch
 
 def handler(event, context):
     logger.info(f"Event: {json.dumps(event)}")
 
     body = json.loads(event["Records"][0]["body"])
     analysis_path = body["ANALYSIS_PATH"]
-    expected_count = body["EXPECTED_COUNT"]
-    start_time = body["START_TIME"]
-    
-    elapsed_mins = (datetime.datetime.now(datetime.timezone.utc).timestamp() - start_time) / 60
-    if elapsed_mins > MAX_POLL_DURATION_MINS:
-        print(f"TIMEOUT: Batch took longer than {MAX_POLL_DURATION_MINS} mins. Stopping poller.")
-        return
-    
-    try:
-        # remove wr here, heavy dependency for not much benefit
-        files = wr.s3.list_objects(analysis_path)
-        parquet_files = [f for f in files if f.endswith('.parquet')]
-        current_count = len(parquet_files)
-    except Exception:
-        current_count = 0
 
-    if current_count >= expected_count:
+    JobBatch(hash_key="snapshot_generation", range_key=JOB_BATCH_SK).update(
+        actions=[
+            Action(JobBatch.completed, Value(1), action='ADD')
+        ]
+    )
+
+    batch = JobBatch.get(hash_key="snapshot_generation", range_key=JOB_BATCH_SK)
+
+    if batch.completed == batch.expected:
         kick_off_next_processing_layer(body["DATASET_NAME"], body["PARTITION_COL"], analysis_path)
-    else:
-        requeue_poller(body)
-        
-def requeue_poller(body):
-    sqs = boto3.client("sqs")
-    queue_url = get_queue_url(sqs, os.environ["SQS_QUEUE_NAME"])
-    sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(body), DelaySeconds=POLL_DELAY_SECONDS)
+
 
 def kick_off_next_processing_layer(dataset_name, partition_col, analysis_path):
     sqs = boto3.client("sqs")
     # TODO alter queue names later
+    # TODO: create mapping based on inputs that maps to a specific queue name, so that i can reuse this as an orchestration 
+    #       piece between components 
+
     queue_url = get_queue_url(sqs, "SocrataSnapshotRollUpQueue")
     
     sqs.send_message(
@@ -57,5 +45,3 @@ def kick_off_next_processing_layer(dataset_name, partition_col, analysis_path):
         }),
     )
 
-def get_queue_url(client, name):
-    return client.get_queue_url(QueueName=name)["QueueUrl"]
