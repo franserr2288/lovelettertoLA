3dfcd5adce6f34e6ea88f5c677c43ed8745abd36
francisco laptop <franserr2288@outlook.com>
2025-12-03 01:14:42 -0800

    shared code moved

--- src/lib/shared/messages/sqs.py
+++ src/lib/shared/messages/sqs.py
@@ -4,8 +4,8 @@ import boto3
 def get_sqs_client_and_url(queue_name, sqs_client=None):
     if not sqs_client:
         sqs_client = boto3.client("sqs")
-    response = sqs_client.get_queue_url(
-        QueueName=queue_name
-    )
-    queue_url = response["QueueUrl"]
+    queue_url = get_queue_url(sqs_client, queue_name)
     return sqs_client, queue_url
+
+def get_queue_url(client, name):
+    return client.get_queue_url(QueueName=name)["QueueUrl"]
