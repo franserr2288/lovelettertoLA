ebc1f9885d8da0adaac073ddf5daf17d0358c135
francisco laptop <franserr2288@outlook.com>
2025-12-06 19:21:09 -0800

    update orchestrator so it can send payloads for esri

--- src/ingestion/orchestrator/orchestrator.py
+++ src/ingestion/orchestrator/orchestrator.py
@@ -1,20 +1,28 @@
 import boto3, json
 
 def handler(event, context):
-    queue_name = event["QUEUE_NAME"]
-    dataset_name = event["DATASET_NAME"]
-    dataset_resource_id = event["DATASET_RESOURCE_ID"]
-    format = event["FORMAT"]
-    partition_col = event["PARTITION_COL"]
 
+    api = event.get("SOURCE")
+    queue_name = event["QUEUE_NAME"]
+
+    payload = {
+        "DATASET_NAME": event["DATASET_NAME"],
+        "DATASET_RESOURCE_ID": event["DATASET_RESOURCE_ID"], 
+        "PARTITION_COL": event["PARTITION_COL"]
+    }
+
+    if api == "ESRI":
+        payload["NEEDS_PROCESSING"] = event.get("NEEDS_PROCESSING", False)
+        payload["LAYERS"] = [] 
 
     sqs_client = boto3.client("sqs")
     response = sqs_client.get_queue_url(
         QueueName=queue_name
     )
+
     queue_url = response["QueueUrl"]
 
     sqs_client.send_message(
         QueueUrl=queue_url,
-        MessageBody=json.dumps({"DATASET_NAME":dataset_name, "DATASET_RESOURCE_ID":dataset_resource_id, "FORMAT":format, "PARTITION_COL":partition_col}),
+        MessageBody=json.dumps(payload),
     )
